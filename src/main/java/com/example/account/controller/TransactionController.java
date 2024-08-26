465359a245b03c9552339a66752f3b242da57aae
hjkim <hjhjgudwls1022@gmail.com>
2024-08-26 21:16:21 +0900

    Implement AOP lock for duplication

--- src/main/java/com/example/account/controller/TransactionController.java
+++ src/main/java/com/example/account/controller/TransactionController.java
@@ -1,5 +1,6 @@
 package com.example.account.controller;
 
+import com.example.account.aop.AccountLock;
 import com.example.account.dto.CancelBalance;
 import com.example.account.dto.QueryTransactionResponse;
 import com.example.account.dto.UseBalance;
@@ -22,6 +23,7 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     @PostMapping("/transaction/use")
+    @AccountLock
     public UseBalance.Response useBalance(
             @Valid @RequestBody UseBalance.Request request
     ) {
@@ -44,11 +46,13 @@ public class TransactionController {
     }
 
     @PostMapping("/transaction/cancel")
+    @AccountLock
     public CancelBalance.Response cancelBalance(
             @Valid @RequestBody CancelBalance.Request request
-    ) {
+    ) throws InterruptedException {
 
         try {
+            Thread.sleep(3000L);
             return CancelBalance.Response.from(
                     transactionService.cancelBalance(
                             request.getTransactionId(),
